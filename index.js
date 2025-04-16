b549feb5aac3ab50259fc0fc2bb8320e05430bda
luxinglong <1835741315@qq.com>
2025-04-16 09:37:12 +0800

    初始化代码

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1. 数组扁平化（Flatten Nested Array）
+// 题目：编写一个函数，将任意层嵌套的数组转换为一维数组。
+// 示例：
+// 输入：[1, [2, [3]]] → 输出：[1, 2, 3]
+// 要求：不用现成的 Array.prototype.flat() 方法。
+const flattenArray = (arr) => {
+
+}
